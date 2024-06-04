37226f8401ae044fd698ddb9c7f432bdd01d5bf1
yololetscode <126184341+yololetscode@users.noreply.github.com>
2024-06-04 08:39:14 -0400

    CreatePFXCert.ps1
    Here is the syntax with the parameters. 1st parameter is for setting password of the .pfx cert, second is location of the non pfx cert. third is the output of the new .pfx cert
    
    .\CreatePfxFromCer.ps1 -password "your_password" -cerPath "C:\path\to\certificate.cer" -pfxOutputPath "C:\path\to\output.pfx"

---
+++ createPFXCert.ps1
@@ -0,0 +1,16 @@
+param (
+    [string]$password,
+    [string]$cerPath,
+    [string]$pfxOutputPath
+)
+
+# Convert plain text password to a secure string
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+
+# Load the .cer file
+$cert = Get-PfxCertificate -FilePath $cerPath
+
+# Export the certificate as .pfx
+$pfxContent = Export-PfxCertificate -Cert $cert -Password $securePassword -FilePath $pfxOutputPath -Force
+
+Write-Host "PFX certificate created at: $pfxOutputPath"
